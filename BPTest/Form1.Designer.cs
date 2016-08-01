33ae5dda57e7180bce6a80dee4ad4b18b265aff4
nelmcbrien <nelmcbrien@gmail.com>
2016-08-01 15:41:46 -0500

    Initial file commit

---
+++ BPTest/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BPTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.cbKey = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Location = new System.Drawing.Point(55, 39);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(75, 23);
+            this.btnToggle.TabIndex = 0;
+            this.btnToggle.Text = "Start";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // cbKey
+            // 
+            this.cbKey.FormattingEnabled = true;
+            this.cbKey.Items.AddRange(new object[] {
+            "E",
+            "K"});
+            this.cbKey.Location = new System.Drawing.Point(55, 12);
+            this.cbKey.Name = "cbKey";
+            this.cbKey.Size = new System.Drawing.Size(75, 21);
+            this.cbKey.TabIndex = 1;
+            this.cbKey.Text = "Select One";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 98);
+            this.Controls.Add(this.cbKey);
+            this.Controls.Add(this.btnToggle);
+            this.Name = "Form1";
+            this.Text = "Presser";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.ComboBox cbKey;
+    }
+}
+
---
+++ BPTest/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace BPTest
+{
+    public partial class Form1 : Form
+    {
+        private string key;
+        private bool run;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnToggle_Click(object sender, EventArgs e)
+        {   
+            if (btnToggle.Text == "Start")
+            {
+                btnToggle.Text = "Stop";
+                run = true;
+                doPresses();
+            }
+            else
+            {
+                btnToggle.Text = "Start";
+                run = false;
+            }
+        
+        }
+
+        async void doPresses()
+        {
+            try
+            {
+                string key = getKey();
+                await Task.Delay(2000);//Wait before hitting key first time
+                while (run)
+                {
+                    SendKeys.Send(key);
+                    await Task.Delay(2000); //Two seconds for testing
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Please select a key.", "Error", MessageBoxButtons.OK);
+            }         
+        }
+
+        private string getKey()
+        {
+            string key = cbKey.SelectedItem.ToString();
+
+            switch(key)
+            {
+                case "E":
+                    return "E";   
+                case "K"  :
+                    return "K";                                
+                default:
+                    return "{ENTER}";
+            }                
+        }
+    }
+}
